f1cda35c9b4387b6070822f02f3642fce0872a61
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2021-12-05 18:24:37 +0100

    Setting up theme

---
+++ lib/common/constants/colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xffFDAB53);
+const primaryColorDarker1 = Color(0xffF69A67);
+const primaryColorDarker2 = Color(0xffE78F5B);
+const primaryColorLessOpacity = Color(0x21FDAB53);
+const primaryColorBorder = Color(0xffFFC88C);
+
+const secondaryColor = Color(0xff3D5382);
+const secondaryColorBrighter = Color(0xff6C80AC);
+const secondaryColorLessOpacity = Color(0x993D5382);
+
+const backgroundColorProduct = Color(0xffEDF6FF);
+const backgroundColorPlanning = Color(0xffF6F6F6);
+
+const lightGreyColor = Color(0xffE8E6E6);
+
+const bodyTextColor = Color(0xffC3C3C3);
+const bodyTextColor2 = Color(0xffCBCBCB);
+
+const inputBackground = Color(0xffEEEEEE);
+const inputBorder = Color(0xffC5CCDA);
+
+const errorMessageColor = Color(0xffE44747);
+const successMessageColor = Color(0xff6FA492);
+const validateColor = Color(0xff59AE05);
---
+++ lib/common/theme.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'constants/colors.dart';
+
+ThemeData basicTheme() {
+  TextTheme _basicTextTheme(TextTheme base) {
+    return base.copyWith(
+      headline1: TextStyle(
+          fontFamily: 'Poppins',
+          color: const Color(0xff3D5382),
+          fontSize: 24,
+          fontWeight: FontWeight.bold),
+      headline2: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 26.0,
+        color: secondaryColor,
+      ),
+      headline3: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 24.0,
+        fontWeight: FontWeight.w600,
+        color: secondaryColor,
+      ),
+      headline4: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 22.0,
+        color: secondaryColor,
+      ),
+      headline5: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 20.0,
+        color: secondaryColor,
+      ),
+      headline6: TextStyle(
+          fontFamily: 'Poppins',
+          fontSize: 18,
+          color: secondaryColorLessOpacity,
+          fontWeight: FontWeight.w400),
+      bodyText1: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 14.0,
+        color: bodyTextColor,
+      ),
+      bodyText2: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 12.0,
+        color: bodyTextColor,
+      ),
+      button: TextStyle(
+        fontFamily: 'Poppins',
+        fontSize: 16.0,
+        color: primaryColor,
+      ),
+    );
+  }
+
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+      textTheme: _basicTextTheme(base.textTheme),
+      //textTheme: Typography().white,
+      primaryColor: primaryColor,
+      appBarTheme: AppBarTheme(backgroundColor: primaryColor),
+      textSelectionTheme: TextSelectionThemeData(
+          cursorColor: primaryColor, selectionColor: primaryColor),
+      //primaryColor: Color(0xff4829b2),
+      indicatorColor: Color(0xFF807A6B),
+      scaffoldBackgroundColor: Color(0xFFF5F5F5),
+      accentColor: Color(0xFFFFF8E1),
+      iconTheme: IconThemeData(
+        color: Colors.white,
+        size: 20.0,
+      ),
+      buttonColor: Colors.white,
+      backgroundColor: Colors.white,
+      tabBarTheme: base.tabBarTheme.copyWith(
+        labelColor: Color(0xffce107c),
+        unselectedLabelColor: Colors.grey,
+      ));
+}
