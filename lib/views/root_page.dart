44ac9a4c522ca546f3cf632f4324fd4638dcafc9
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-12-07 10:40:21 +0100

    Import assets

--- lib/views/root_page.dart
+++ lib/views/root_page.dart
@@ -1,4 +1,4 @@
-import 'package:festivalapp/services/api/repositories/user/UserFetcher.dart';
+import 'package:festivalapp/services/api/repositories/user/user_fetcher.dart';
 import 'package:festivalapp/views/home/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:festivalapp/common/constants/colors.dart';
