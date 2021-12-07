9b9669f939c2f2a02439477fc97d4ea018aa4b95
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-12-07 09:52:36 +0100

    Update app_user.dart

--- lib/model/app_user.dart
+++ lib/model/app_user.dart
@@ -9,33 +9,37 @@ AppUser appUserFromJson(String str) => AppUser.fromJson(json.decode(str));
 String appUserToJson(AppUser data) => json.encode(data.toJson());
 
 class AppUser {
-  AppUser({
-    required this.id,
-    required this.name,
-    required this.firstName,
-    required this.email,
-  });
+    AppUser({
+        required this.id,
+        required this.name,
+        required this.firstname,
+        required this.email,
+        required this.roles,
+        required this.jwt,
+    });
 
-  String id;
-  String name;
-  String firstName;
-  String email;
+    int id;
+    String name;
+    String firstname;
+    String email;
+    List<String> roles;
+    String jwt;
 
-  String get uid {
-    return id;
-  }
-
-  factory AppUser.fromJson(Map<String, dynamic> json) => AppUser(
+    factory AppUser.fromJson(Map<String, dynamic> json) => AppUser(
         id: json["id"],
         name: json["name"],
-        firstName: json["firstName"],
+        firstname: json["firstname"],
         email: json["email"],
-      );
+        roles: List<String>.from(json["roles"].map((x) => x)),
+        jwt: json["jwt"],
+    );
 
-  Map<String, dynamic> toJson() => {
+    Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
-        "firstName": firstName,
+        "firstname": firstname,
         "email": email,
-      };
+        "roles": List<dynamic>.from(roles.map((x) => x)),
+        "jwt": jwt,
+    };
 }
