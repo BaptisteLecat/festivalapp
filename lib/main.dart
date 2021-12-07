c5aaf35dabe0614f087ded6dcb7683d8b4dada1c
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2021-12-07 22:24:16 +0100

    Update main.dart
    Set dev environnement for testing local api calls

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:festivalapp/views/splash_screen_wrapper.dart';
 import 'package:festivalapp/common/theme.dart';
+
+///Development
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(MyApp());
+
+  ///dev
+  HttpOverrides.global = MyHttpOverrides();
 }
 
 class MyApp extends StatelessWidget {
