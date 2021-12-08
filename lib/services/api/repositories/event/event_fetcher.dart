45d1902f275110b70775fdbeaaa3c4b557b90c2c
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2021-12-08 19:50:22 +0100

    Create event_fetcher.dart

---
+++ lib/services/api/repositories/event/event_fetcher.dart
@@ -0,0 +1,10 @@
+import 'package:festivalapp/model/event.dart';
+import 'package:festivalapp/services/api/main_fetcher.dart';
+
+class EventFetcher extends MainFetcher {
+  Future<List<Event>> getEventList() async {
+    final response = await this.get("events");
+    print(response);
+    return listEventFromJson(response);
+  }
+}
