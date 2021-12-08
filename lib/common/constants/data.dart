311afaa3f674b4c24046df106c4a28fced57582a
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2021-12-08 19:50:46 +0100

    Create data.dart

---
+++ lib/common/constants/data.dart
@@ -0,0 +1,24 @@
+const List<String> const_weekDayLabel = [
+  "Lun",
+  "Mar",
+  "Mer",
+  "Jeu",
+  "Ven",
+  "Sam",
+  "Dim"
+];
+
+const List<String> const_monthLabel = [
+  "Janvier",
+  "Février",
+  "Mars",
+  "Avril",
+  "Mai",
+  "Juin",
+  "Juillet",
+  "Août",
+  "Septembre",
+  "Octobre",
+  "Novembre",
+  "Décembre",
+];
