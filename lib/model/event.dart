cc2ddfcc4385d195ff1a746ce6b53676f7853664
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2021-12-08 19:50:14 +0100

    Update event.dart
    Adding picture, name and enddate

--- lib/model/event.dart
+++ lib/model/event.dart
@@ -2,8 +2,14 @@
 //
 //     final event = eventFromJson(jsonString);
 
+import 'dart:typed_data';
+
+import 'package:festivalapp/common/constants/data.dart';
 import 'package:meta/meta.dart';
 import 'dart:convert';
+
+List<Event> listEventFromJson(dynamic str) =>
+    List<Event>.from(str.map((x) => Event.fromJson(x)));
 
 Event eventFromJson(String str) => Event.fromJson(json.decode(str));
 
@@ -14,24 +20,51 @@ class Event {
     required this.id,
     required this.artists,
     required this.date,
+    required this.picture,
+    required this.name,
+    required this.endDate,
   });
 
   int id;
   List<Artist> artists;
   DateTime date;
+  String name;
+  String? picture;
+  DateTime endDate;
 
   factory Event.fromJson(Map<String, dynamic> json) => Event(
-        id: json["id"],
-        artists:
-            List<Artist>.from(json["artists"].map((x) => Artist.fromJson(x))),
-        date: DateTime.parse(json["date"]),
-      );
+      id: json["id"],
+      artists:
+          List<Artist>.from(json["artists"].map((x) => Artist.fromJson(x))),
+      date: DateTime.parse(json["date"]),
+      name: json["name"],
+      picture: json["picture"],
+      endDate: DateTime.parse(json["endDate"]));
 
   Map<String, dynamic> toJson() => {
         "id": id,
         "artists": List<dynamic>.from(artists.map((x) => x.toJson())),
         "date": date.toIso8601String(),
+        "name": name,
+        "picture": (picture != null) ? picture : "",
+        "endDate": endDate.toIso8601String(),
       };
+
+  String _getMonth(int index) {
+    return const_monthLabel[index - 1];
+  }
+
+  String getDateOfEvent() {
+    return "${date.day} - ${endDate.day} ${_getMonth(endDate.month)}, ${endDate.year}";
+  }
+
+  Uint8List getPictureEncoded() {
+    if (picture != null) {
+      return const Base64Decoder().convert(picture!);
+    } else {
+      throw Exception("Aucune image disponible");
+    }
+  }
 }
 
 class Artist {
