7724e52e6a3d61abb9477335a85ec3ef3b56cc93
BaptisteLecat <60200125+BaptisteLecat@users.noreply.github.com>
2021-12-26 16:52:09 +0100

    Update ticket_page.dart

--- lib/views/ticket/ticket_page.dart
+++ lib/views/ticket/ticket_page.dart
@@ -18,155 +18,208 @@ class _TicketPageState extends State<TicketPage> {
           itemCount: 4,
           itemBuilder: (BuildContext context, int index) {
             return Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20)),
+                decoration:
+                    BoxDecoration(borderRadius: BorderRadius.circular(20)),
                 child: Padding(
-                  padding: const EdgeInsets.all(18.0),
+                  padding: const EdgeInsets.symmetric(vertical: 18),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Flexible(
                         flex: 2,
-                        child: SizedBox(
-                            child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(
-                              "TomorrowLand",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline1!
-                                  .copyWith(color: secondaryColor),
-                            ),
-                            Text("Montrez ce ticket à l'entrée",
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                          child: SizedBox(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(
+                                "TomorrowLand",
                                 style: Theme.of(context)
                                     .textTheme
-                                    .headline5!
-                                    .copyWith(
-                                        color: secondaryColorLessOpacity)),
-                            Divider(
-                              thickness: 2,
-                            )
-                          ],
-                        )),
+                                    .headline1!
+                                    .copyWith(color: secondaryColor),
+                              ),
+                              Text("Montrez ce ticket à l'entrée",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline5!
+                                      .copyWith(
+                                          color: secondaryColorLessOpacity)),
+                              Divider(
+                                thickness: 2,
+                              )
+                            ],
+                          )),
+                        ),
                       ),
                       Expanded(
-                          flex: 6,
-                          child: Flex(direction: Axis.vertical, children: [
-                            SizedBox(
-                                child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text("Lieu",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline5!
-                                        .copyWith(
-                                            color: secondaryColorLessOpacity)),
-                                Text("New York, Madison Square Garden",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .headline5!
-                                        .copyWith(color: secondaryColor))
-                              ],
-                            )),
-                            SizedBox(
-                                child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Flexible(
-                                  child: Column(
+                          flex: 4,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 18.0),
+                            child: Flex(
+                                direction: Axis.vertical,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  SizedBox(
+                                      child: Column(
                                     crossAxisAlignment:
                                         CrossAxisAlignment.start,
                                     children: [
-                                      Text("Date",
+                                      Text("Lieu",
                                           style: Theme.of(context)
                                               .textTheme
                                               .headline5!
                                               .copyWith(
                                                   color:
                                                       secondaryColorLessOpacity)),
-                                      Text("10-03-2022",
+                                      Text("New York, Madison Square Garden",
                                           style: Theme.of(context)
                                               .textTheme
                                               .headline5!
                                               .copyWith(color: secondaryColor))
                                     ],
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                                  )),
+                                  SizedBox(
+                                      child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Text("Place",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline5!
-                                              .copyWith(
-                                                  color:
-                                                      secondaryColorLessOpacity)),
-                                      Text("101",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline5!
-                                              .copyWith(color: secondaryColor))
+                                      Flexible(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text("Date",
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headline5!
+                                                    .copyWith(
+                                                        color:
+                                                            secondaryColorLessOpacity)),
+                                            Text("10-03-2022",
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headline5!
+                                                    .copyWith(
+                                                        color: secondaryColor))
+                                          ],
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text("Place",
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headline5!
+                                                    .copyWith(
+                                                        color:
+                                                            secondaryColorLessOpacity)),
+                                            Text("101",
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headline5!
+                                                    .copyWith(
+                                                        color: secondaryColor))
+                                          ],
+                                        ),
+                                      ),
                                     ],
-                                  ),
-                                ),
-                              ],
-                            )),
-                            SizedBox(
-                                child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Flexible(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
+                                  )),
+                                  SizedBox(
+                                      child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Text("Prix",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline5!
-                                              .copyWith(
-                                                  color:
-                                                      secondaryColorLessOpacity)),
-                                      Text("346€",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline5!
-                                              .copyWith(color: secondaryColor))
+                                      Flexible(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text("Prix",
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headline5!
+                                                    .copyWith(
+                                                        color:
+                                                            secondaryColorLessOpacity)),
+                                            Text("346€",
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headline5!
+                                                    .copyWith(
+                                                        color: secondaryColor))
+                                          ],
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text("Commande",
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headline5!
+                                                    .copyWith(
+                                                        color:
+                                                            secondaryColorLessOpacity)),
+                                            Text("234656",
+                                                style: Theme.of(context)
+                                                    .textTheme
+                                                    .headline5!
+                                                    .copyWith(
+                                                        color: secondaryColor))
+                                          ],
+                                        ),
+                                      ),
                                     ],
-                                  ),
-                                ),
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text("Commande",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline5!
-                                              .copyWith(
-                                                  color:
-                                                      secondaryColorLessOpacity)),
-                                      Text("234656",
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headline5!
-                                              .copyWith(color: secondaryColor))
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ))
-                          ])),
-                      Divider(
-                        thickness: 2,
+                                  ))
+                                ]),
+                          )),
+                      Flexible(
+                        flex: 1,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              height: 40,
+                              width: 22,
+                              decoration: const BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: BorderRadius.only(
+                                      topRight: Radius.circular(20),
+                                      bottomRight: Radius.circular(20))),
+                            ),
+                            const Divider(
+                              thickness: 2,
+                            ),
+                            Container(
+                              height: 40,
+                              width: 22,
+                              decoration: const BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(20),
+                                      bottomLeft: Radius.circular(20))),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                          child: Container(
+                            color: Colors.purple,
+                          ),
+                        ),
                       )
                     ],
                   ),
