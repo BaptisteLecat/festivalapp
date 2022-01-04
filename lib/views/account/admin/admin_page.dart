f2256e63284e18a6a2b5cf6c16fcf59040dd0ef4
Lecat Baptiste <60200125+BaptisteLecat@users.noreply.github.com>
2022-01-04 15:54:08 +0100

    Handle admin menu

---
+++ lib/views/account/admin/admin_page.dart
@@ -0,0 +1,73 @@
+import 'package:festivalapp/common/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class AdminPage extends StatefulWidget {
+  const AdminPage({Key? key}) : super(key: key);
+
+  @override
+  _AdminPageState createState() => _AdminPageState();
+}
+
+class _AdminPageState extends State<AdminPage> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    const List<Widget> _pages = <Widget>[
+      Icon(
+        Icons.call,
+        size: 150,
+      ),
+      Icon(
+        Icons.camera,
+        size: 150,
+      ),
+      Icon(
+        Icons.chat,
+        size: 150,
+      ),
+    ];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Panel Administrateur"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: secondaryColor,
+        selectedItemColor: primaryColor,
+        currentIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.event),
+              activeIcon: Icon(
+                Icons.event,
+                color: primaryColor,
+              ),
+              label: 'Evenement'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.music_note),
+              activeIcon: Icon(
+                Icons.music_note,
+                color: primaryColor,
+              ),
+              label: 'Genre-Musique'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.album),
+              activeIcon: Icon(
+                Icons.album,
+                color: primaryColor,
+              ),
+              label: 'Artiste'),
+        ],
+      ),
+      body: SafeArea(
+          child: IndexedStack(
+        index: selectedIndex,
+        children: _pages,
+      )),
+    );
+  }
+}
--- lib/views/account/components/admin_button.dart
+++ lib/views/account/components/admin_button.dart
@@ -1,4 +1,5 @@
 import 'package:festivalapp/common/constants/colors.dart';
+import 'package:festivalapp/views/account/admin/admin_page.dart';
 import 'package:flutter/material.dart';
 
 class AdminButton extends StatelessWidget {
@@ -6,12 +7,18 @@ class AdminButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          color: primaryColor,
-          borderRadius: BorderRadius.all(Radius.circular(18))),
-      child: const Center(
-        child: Text("Panel Administrateur"),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => AdminPage()));
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+            color: primaryColor,
+            borderRadius: BorderRadius.all(Radius.circular(18))),
+        child: const Center(
+          child: Text("Panel Administrateur"),
+        ),
       ),
     );
   }
